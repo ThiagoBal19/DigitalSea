ea1c8402e989e54fb19f74284a59456c9c9c9be3
Thiago Balderrama <tbalderrama8@gmail.com>
2021-12-01 16:47:53 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import './scss/styles.css';
+import Navbar from './utils/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <Navbar/>
+      <br/>
+      <p className="App"><strong>*Página en desarrollo...</strong></p>
+    </header>
   );
 }
 
---
+++ src/utils/Navbar.js
@@ -0,0 +1,37 @@
+import brandWallpaper from '../components/assets/brandWallpaper.png';
+
+const Navbar = () => {
+    return(
+        <>
+            <section id="encabezado">
+                <div className="sub-encabezado">
+                    <a href="x"><img src={brandWallpaper} alt="ilustración"/></a>
+                    <ul className="listas">
+                        <li>
+                            <a href="x">HOME</a>
+                        </li>
+                        <li>
+                            <a href="x">CARRITO</a>
+                        </li>
+                    </ul>
+                </div>
+                
+                <div className="sub-encabezado2">
+                    <ul className="listas">
+                        <li>
+                            <a href="x">EXPLORAR</a>
+                        </li>
+                        <li>
+                            <a href="x">NFT's</a>
+                        </li>
+                        <li>
+                            <a href="x">ARTE</a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Navbar;
